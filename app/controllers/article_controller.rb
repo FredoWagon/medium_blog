e317d341885cb1803820b1673fc4715561b106c1
fredowagon <fedde.leg@gmail.com>
2021-07-10 19:49:46 +0200

    Créer controlleur Article + installation bootstrap + creation composant article_card

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,6 +1,6 @@
 class ArticleController < ApplicationController
   def index
-
+    @articles = Article.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'article#index'
 
-  resources :articles
+  resources :article
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
